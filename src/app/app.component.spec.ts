959a3ac207fdde09c80d6a458899a577bbf7bee5
Imanuel Titar Nugroho <imanuelnugroho@gmail.com>
2025-05-20 16:16:13 +0000

    commit 063 - resolve base url naming

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'flight-booking-frontend' title`, () => {
+  it(`should have the 'flightbookingsystem' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('flight-booking-frontend');
+    expect(app.title).toEqual('flightbookingsystem');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, flight-booking-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, flightbookingsystem');
   });
 });
