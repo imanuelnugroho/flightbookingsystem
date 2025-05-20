0380646e1bb07b9c97cd20cd8c4c7c5538d228ae
Imanuel Titar Nugroho <imanuelnugroho@gmail.com>
2025-05-20 18:58:27 +0000

    Updates

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -25,11 +25,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5627, hash: '7ab6730a7797ba926c42be59f2667708c794bf0a1ae75ea33b3de3e186598301', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1236, hash: '575d2a9248b5493ed98ebd0071370b8ca0ece2ffea2c9b0e95674a1be638696e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 35983, hash: 'd084ce5a2e05a3ab0aaa566e235dce125b14ccaba3f0b71ec6211e48924fccb7', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'search-results/index.html': {size: 12136, hash: 'cf864c2bcd7b7229f6337aec4121bf96a7be72dcd05a1118c3f92a785f21fa6b', text: () => import('./assets-chunks/search-results_index_html.mjs').then(m => m.default)},
-    'confirmation/index.html': {size: 12110, hash: '80db36f7df8cf9f938fd4e232a97d76f7d50dfed9a26d5efa3e12c451a4ca06f', text: () => import('./assets-chunks/confirmation_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5627, hash: 'e10d2a7c576f2a12a6720fa30199781cec1feb609129c4705f7c349a6cc097d9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1236, hash: '59dc54cc024d97a9d4b88e6c12a640e23d9dcdb65d1048b204f0b23c1e8daca6', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 15357, hash: 'b7c5fecd45b50bb84e33d95349bef1fb240e0db3b44ada7578332829364f40bd', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'search-results/index.html': {size: 8155, hash: 'e333e602e88ef5e2e1e09edc845c6bddd3ed0be558a1c9d02b1a98c7371b2fb8', text: () => import('./assets-chunks/search-results_index_html.mjs').then(m => m.default)},
+    'confirmation/index.html': {size: 8280, hash: 'd9aaae62e230ba83b8bcbd0f9ead40eabd21f38b0b1e5afab2164d3170e42af6', text: () => import('./assets-chunks/confirmation_index_html.mjs').then(m => m.default)},
     'styles-JSBOE537.css': {size: 238910, hash: 'RY1TK5en6C8', text: () => import('./assets-chunks/styles-JSBOE537_css.mjs').then(m => m.default)}
   },
 };
