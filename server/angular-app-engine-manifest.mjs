0acb44b1644a74274ed637f2b05f8f764b32f7dd
Imanuel Titar Nugroho <imanuelnugroho@gmail.com>
2025-05-25 19:37:56 +0000

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/flightbookingsystem',
+  basePath: 'https://imanuelnugroho.github.io/flightbookingsystem',
   supportedLocales: {
   "en-US": ""
 },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,7 +2,7 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/flightbookingsystem/',
+  baseHref: 'https://imanuelnugroho.github.io/flightbookingsystem/',
   locale: undefined,
   routes: [
   {
@@ -25,11 +25,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5301, hash: 'a6b847f552b8527fa491adc49c4a3def212bcea74cd15c8779bc074218ad3038', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1137, hash: 'e31b907ff0f9567c4c8e9fd974a75f1437da37424d597de9c2e086c9976f8128', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 14589, hash: '8dd1a90565de22739b19f1bb4b054648621992c031c133817e43ad5118d26be2', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'search-results/index.html': {size: 11357, hash: '03695384335ad40aa86812e42183622439848154f31e27fcde437567008247ee', text: () => import('./assets-chunks/search-results_index_html.mjs').then(m => m.default)},
-    'confirmation/index.html': {size: 11453, hash: '5f7f2380e80da4bf60b0145fdda974efa1fd62275943cd8cb1df2c54d165f214', text: () => import('./assets-chunks/confirmation_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5333, hash: '05a80796ee239168513da034e5c92d143d96972feb7b39303135a145bd76dbfa', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1169, hash: '03cedde5f31e9d50bf2f984d883b628522e1333dcf5ea7d97230547ed74dbbc0', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 14621, hash: '412a3ed5ae1f0cd0b1fd8674c99068dfe95db2a02d53a9343ace93217d152396', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'search-results/index.html': {size: 11389, hash: '60aafe5e7cd5df04e640bb814b1437ef8eda244fdb4396a03fcf57bcf530b4d5', text: () => import('./assets-chunks/search-results_index_html.mjs').then(m => m.default)},
+    'confirmation/index.html': {size: 11485, hash: 'aa4be25e30afb0cbfccd48411ec2daa167e24a54e7a1eb1d0aca8e22c76bebf8', text: () => import('./assets-chunks/confirmation_index_html.mjs').then(m => m.default)},
     'styles-HTFLABQQ.css': {size: 239011, hash: '4pMjUMAcXV4', text: () => import('./assets-chunks/styles-HTFLABQQ_css.mjs').then(m => m.default)}
   },
 };
